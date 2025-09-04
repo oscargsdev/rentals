733e514afa4cb0f8cc354c520c35631555a363a2
Oscar GS <oscargs.dev@gmail.com>
2025-09-03 22:20:47 -0600

    Path param for viewing a specific property by ID

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Howdy, gimme a house to rent please."))
 }
 
+func registerProperty(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Registering a new property..."))
+}
+
+func viewProperty(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Viewing property with ID %d", id)
+	w.Write([]byte(msg))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/property/register", registerProperty)
+	mux.HandleFunc("/property/view/{id}", viewProperty)
 
 	log.Print("starting server on :4001")
 
